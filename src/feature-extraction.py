d540e5c97029bd10fa65128e594a5bf38c24c85e
Matheus Pereira Dias <matheuspd07@gmail.com>
2025-06-13 11:12:30 -0300

    add initial dinov2 feature extraction

--- src/feature-extraction.py
+++ src/feature-extraction.py
@@ -5,7 +5,14 @@ import numpy as np
 import os
 import pyarrow as pa
 from PIL import Image
+from transformers import AutoImageProcessor, AutoModel
+import torch
+from tqdm import tqdm
 
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+processor = AutoImageProcessor.from_pretrained("facebook/dinov2-base")
+model = AutoModel.from_pretrained("facebook/dinov2-base").to(device)
+model.eval()
 
 def extract_features_random(image_path):
     if not isinstance(image_path, str) or not os.path.exists(image_path):
@@ -15,8 +22,52 @@ def extract_features_random(image_path):
     img.histogram()
     return np.random.rand(512).tolist()
 
+def extract_features_from_folder(folder_path, extensions={".jpg", ".png", ".jpeg"}):
+    features = []
+    image_paths = []
+    for file in tqdm(os.listdir(folder_path)):
+        if os.path.splitext(file)[1].lower() in extensions:
+            path = os.path.join(folder_path, file)
+            try:
+                feat = extract_features_dinov2(path)
+                features.append(feat)
+                image_paths.append(file)
+            except Exception as e:
+                print(f"Error at {file}: {e}")
+    return features, image_paths
+
+def extract_features_dinov2(image_path):
+    if not isinstance(image_path, str) or not os.path.exists(image_path):
+        return None
+
+    img = Image.open(image_path)
+    img.histogram()
+    print(img.height, img.width)
+
+    inputs = processor(images=img, return_tensors="pt").to(device)
+    print(inputs.pixel_values.shape)
+
+    with torch.no_grad():
+        outputs = model(**inputs)
+    
+    cls_token = outputs.last_hidden_state[:, 0, :]
+    print(cls_token.shape)
+
+    feature_vector = cls_token.squeeze(0).cpu().numpy()
+
+    return feature_vector
+    
 
 if __name__ == "__main__":
+
+    # Image folder
+    # folder = "./imgs"
+    # features, paths = extract_features_from_folder(folder)
+    # features_np = np.array(features)
+
+    # print("Shape: ", features_np.shape)
+    # print(features_np)
+
     client = Client(n_workers=6, threads_per_worker=4, memory_limit="5GB")
 
     df = dask.dataframe.read_csv("./data/image-files.csv")
